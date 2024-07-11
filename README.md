4037cc144e9d87072352633a6553eabfca6cd91d
Gravity2333 <45278063+Gravity2333@users.noreply.github.com>
2024-07-11 18:16:34 +0800

    Add files via upload

--- README.md
+++ README.md
@@ -1,2 +1,53 @@
-# 手动实现一个react-redux connect函数和Provide
-# my-redux-connect
+# 手动实现一个react-redux connect函数
+##  my-redux-connect 使用方法
+
+ ```javascript
+// APP.js
+import { Provider } from "my-redux-connect";
+import Home from "./pages/Home";
+import { store } from "./store";
+
+function App() {
+  return (
+    <Provider store={store}>
+      <Home/>
+    </Provider>
+  );
+}
+
+export default App;
+
+// Home.js
+import { connect } from "my-redux-connect";
+import { addCounterNumAction } from "../../store/counter";
+
+function Home({num,addCounterNum}) {
+  return (
+    <div
+      style={{
+        height: "150px",
+        backgroundColor: "lightgray",
+        border: "1px solid black",
+        textAlign: "center",
+      }}
+    >
+      <h2>Home</h2>
+      <span>counter: {num}</span>
+      <div>
+        <button onClick={()=>addCounterNum(1)}>+1</button>
+        <button onClick={()=>addCounterNum(3)}>+3</button>
+        <button onClick={()=>addCounterNum(5)}>+5</button>
+      </div> 
+    </div>
+  );
+}
+
+const mapStateToProps = ({counter: {num}})=>({num})
+const mapDispatchToProps = dispatch => ({
+    addCounterNum: num=>dispatch(addCounterNumAction(num))
+})
+
+export default connect(mapStateToProps,mapDispatchToProps)(Home);
+
+```
+
--- src/index.js
+++ src/index.js
@@ -1,7 +1,49 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import App from './App';
+import React, { PureComponent } from "react";
+import { createContext } from "react";
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(<App />);
+export const MyConnectContext = createContext({});
 
+export const Provider = React.memo(function ({ store, children }) {
+  return (
+    <MyConnectContext.Provider value={store}>
+      {children}
+    </MyConnectContext.Provider>
+  );
+});
+
+export function connect(
+  mapStateToProps = () => ({}),
+  mapDispatchToProps = () => ({})
+) {
+  return (Cpn) => {
+    class WrapperCpn extends PureComponent {
+      constructor(props = {},context={}) {
+        super(props);
+        this.state = mapStateToProps(context.getState())||{};
+      }
+
+      componentDidMount() {
+        this.unsubscribe = this.context.subscribe(() => {
+          this.setState(mapStateToProps(this.context.getState()));
+        });
+      }
+
+      componentWillUnmount() {
+        this.unsubscribe();
+      }
+
+      render() {
+        const dispatchFunsObj = mapDispatchToProps(this.context.dispatch);
+        return (
+          <Cpn
+            {...(this.props || {})}
+            {...(this.state || {})}
+            {...(dispatchFunsObj || {})}
+          />
+        );
+      }
+    }
+    WrapperCpn.contextType = MyConnectContext;
+    return WrapperCpn;
+  };
+}
